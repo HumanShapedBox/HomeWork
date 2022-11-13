2861d355b331bbe1f625304fda1284c52660e351
HumanBox <stashksenia@gmail.com>
2022-11-13 18:12:20 +0300

    HW3 Done

---
+++ HW3_ChetNechet/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if ((a%2) == 0)
+{
+    Console.WriteLine("Это чётное число");
+}
+else
+{
+    Console.WriteLine("Это нечётное число");
+}
