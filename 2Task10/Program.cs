96b43d3a09be9d0c4778bd5b71f68b399b251336
HumanBox <stashksenia@gmail.com>
2022-11-16 11:36:29 +0300

    Task Show Second Number

---
+++ 2Task10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа. 
+// Выполнить с помощью числовых операций.
+
+Console.WriteLine("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int ShowSecondNumber(int number)
+{
+    int twoFigNum = num % 100;
+    int result =  twoFigNum / 10;
+    return result;
+}
+Console.WriteLine(ShowSecondNumber(num));
