094732f8bb1e353275099902ebf8528701a95acb
HumanBox <stashksenia@gmail.com>
2022-11-17 11:19:50 +0300

    Task13 Third Number

--- 2Task13/Program.cs
+++ 2Task13/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+// Выполнить с помощью числовых операций.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int ThirdNum(int number)
+{
+   while(num >= 999) num /= 10;
+   int result = num % 10;
+   return result;
+}
+
+Console.WriteLine(ThirdNum(num));
