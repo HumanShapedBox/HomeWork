38da7c69d6dbe54940e01d45001ff74e439d7e3c
HumanBox <stashksenia@gmail.com>
2022-11-17 11:49:52 +0300

    Task15 Weekend

---
+++ Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите номер дня недели: ");
+string day = Console.ReadLine();
+
+if(day == "1") Console.WriteLine("Будний день");
+else if(day == "2") Console.WriteLine("Будний день");
+else if(day == "3") Console.WriteLine("Будний день");
+else if(day == "4") Console.WriteLine("Будний день");
+else if(day == "5") Console.WriteLine("Будний день");
+else if(day == "6") Console.WriteLine("Выходной");
+else if(day == "7") Console.WriteLine("Выходной");
+else Console.WriteLine("Такого дня недели нет");
