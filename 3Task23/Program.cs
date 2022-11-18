574767bf2039f6db685866e3d999959eccda6174
HumanBox <stashksenia@gmail.com>
2022-11-18 13:37:52 +0300

    Task23 Cube

---
+++ 3Task23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32 (Console.ReadLine());
+
+void Cube (int num)
+{
+    int count = 0;
+    while(count <= num)
+    {
+        Console.WriteLine($"{count}\t{count * count * count}");
+        count++;
+    }
+}
+
+Console.WriteLine("Число\tКуб");
+Cube (n);
