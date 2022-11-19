e276c87bcbe27c1127e93f12dd497b96df5f9401
HumanBox <stashksenia@gmail.com>
2022-11-19 11:38:21 +0300

    Task19 Palindrome

---
+++ 3Task19/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций.
+
+Console.WriteLine("Введите пятизначное число (12345): ");
+int num = Convert.ToInt32(Console.ReadLine());
+int digit = 0;
+int nReverse = 0;
+int reverse = Reverse(num);
+if(num > 9999 && num < 100000) Console.WriteLine(Palindrome(num, reverse) ? "Число является палиндромом" : "Число не является палиндромом");
+else Console.WriteLine("Некорректный ввод");
+
+int Reverse(int number)
+{
+    while(number > 0)
+    {
+        digit = number % 10;
+        nReverse = nReverse * 10 + digit;
+        number = number / 10;
+    }
+    return nReverse;
+}
+
+bool Palindrome(int number, int reverse)
+{
+    return (number == reverse);
+}
