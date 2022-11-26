d93092de110c98ac57e20f4ab83ea5037da45eb9
HumanBox <stashksenia@gmail.com>
2022-11-26 14:41:56 +0300

    Task36

---
+++ 5Task36/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте одномерный массив, 
+// заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt (int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max +1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length -1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int SumOddIndex(int[] arr)
+{
+    int count = default;
+    for(int i = 1; i < arr.Length; i+=2)
+    {
+        count += arr[i];
+    }
+    return count;
+}
+
+Console.WriteLine("Укажите длинну массива (например, 10): ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArrayRndInt (length, -100, 101);
+PrintArray(array);
+int oddIndexSum = SumOddIndex(array);
+Console.WriteLine($"Сумма чисел, стоящих в массиве на нечётных позициях = {oddIndexSum}");
