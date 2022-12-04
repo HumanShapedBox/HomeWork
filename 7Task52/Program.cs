ea31a6d9f5b80d81cba416f0fec4da7a5dd7bfba
HumanBox <stashksenia@gmail.com>
2022-12-04 14:55:25 +0300

    Task52

--- 7Task52/Program.cs
+++ 7Task52/Program.cs
@@ -40,31 +40,32 @@ void PrintMatrix(int[,] matrix)
 double[] ColumnMean(int[,] matrix)
 {
     int size = matrix.GetLength(1);
+    int elements = matrix.GetLength(0);
     double[] meanColumns = new double[size];
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
         for (int i = 0; i < matrix.GetLength(0); i++)
         {
-            meanColumns[i] += matrix[i, j] / matrix.GetLength(0);
-            meanColumns[i] = Math.Round(meanColumns[i], 1, MidpointRounding.ToZero);
+            meanColumns[j] += matrix[i, j];
         }
+        meanColumns[j] = meanColumns[j] / elements;
+        meanColumns[j] = Math.Round(meanColumns[j], 1, MidpointRounding.ToZero);
     }
     return  meanColumns;
 }
 
 void PrintArray(double[] arr)
 {
-    Console.Write("[");
+    Console.Write("Среднее арифметическое каждого столбца: ");
     for(int i = 0; i < arr.Length; i++)
     {
         if(i < arr.Length -1) Console.Write($"{arr[i]}, ");
         else Console.Write($"{arr[i]}");
     }
-    Console.WriteLine("]");
+    Console.WriteLine();
 }
 
 int[,] array = CreateMatrixRndInt(3, 5, 0, 11);
 PrintMatrix(array);
 double[] arrayColumnMean = ColumnMean(array);
-Console.WriteLine("Среднее арифметическое каждого столбца: ");
 PrintArray(arrayColumnMean);
